bb64a2d22d7680328eac3f12830a19b9318bd7ad
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-05-06 03:50:07 +0530

    added image

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { Section, Div } from "../shared";
 export default function Logo() {
   return (
     <Section className="">
-        <div className="w-11/12 mx-auto max-w-6xl py-2">
+        <div className="w-11/12 mx-auto max-w-7xl py-4">
             <div className="font-poppins">
                 <h1 className="font-bold">
                     Github 
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,18 @@
-// import backgroundImg from "../images/backgroundImg.png";
+import bg from "../images/bg.png";
 import { Section } from "../shared";
 
 function Nav() {
   return (
-    <Section className="border">
-      <div className="bg-[url('../images/backgroundImg.png')] w-11/12 mx-auto max-w-6xl h-full py-8">
+    <Section className="">
+      <div 
+        className="w-11/12 mx-auto max-w-7xl h-full rounded-md py-[40px] md:py-16" 
+        style={{
+          backgroundImage: `url(${bg})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat'
+         }}
+      >
         
       </div>
     </Section>
